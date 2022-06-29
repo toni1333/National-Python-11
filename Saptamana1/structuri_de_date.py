ae1c72186c0fd549391dfb4ad9fb88f35b364f46
toni <antoniebohak@yahoo.com>
2022-06-29 21:30:39 +0300

    liste

---
+++ Saptamana1/structuri_de_date.py
@@ -0,0 +1,16 @@
+# structuri de date
+my_list = [8, 2, 'a', '4']
+print('mesaj')
+# print(type(my_list)) # tipul unei variabile
+# print(my_list[2])
+# print(my_list[3])
+# print(my_list[-1])
+# print(len(my_list))
+# my_list.append('ANA')
+# print(my_list)
+# print(my_list.index(8))
+# my_list.insert(-1, 'A')
+# print(my_list[-2: ])
+# print(my_list[7:9])
+my_list.append(['A' , 'B', 3, 'x', [4, 7,'w']])
+print(my_list)
