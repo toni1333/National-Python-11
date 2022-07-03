23171470518f26c52d8bd4520be81d35d1e6ab8e
toni <antoniebohak@yahoo.com>
2022-07-03 15:38:38 +0300

    `

--- Saptamana1/variabile.py
+++ Saptamana1/variabile.py
@@ -13,7 +13,58 @@
      4 afisare rezultate
 """
 
-venit_pe_ora = float(input("Introduce-ti venitul pe ora:"))
-nr_ore =float(input("Introduce-ti nr de ore:"))
-venit_saptamanal = venit_pe_ora * nr_ore
-print("Venitul saptamanal este de " + str(venit_saptamanal) + " lei")
+# venit_pe_ora = float(input("Introduce-ti venitul pe ora:"))
+# # nr_ore =float(input("Introduce-ti nr de ore:"))
+# venit_saptamanal = venit_pe_ora * nr_ore
+# print("Venitul saptamanal este de " + str(venit_saptamanal) + " lei")
+
+
+# teste acasa...
+
+a= 1 ; b= 2 ; c= a % b
+if a is a and b< c:
+    print('True')
+else:
+    print('False')
+print('-\n-\n-')
+print (c)
+print('-\n-\n-')
+
+# s=b"Hello World"
+# n=[1,2,3,4,5,6]
+# # if 'o' in s or 0 in n:
+# #     print('              True ')
+# # else:
+# #     print('              False')
+# print(s)
+#
+#
+# s1=b"Hello World"
+# if s << s1:
+#     print('biti true')
+# else:
+#     print('biti false')
+
+
+a=3
+child_age=3
+print(id(child_age))
+
+print('-\n-\n-')
+
+my_var=100
+my_var >>= 1
+print(my_var)
+print(ord('z'))
+print(chr(33))
+
+print('-\n-\n-')
+
+print("Si Dumnezeu a zis: \"Sa se faca lumina!\" ")
+
+print("'\t' aaaa")
+print('-\n-\n-')
+
+print(F"For only {49:.2f} dollars! {1}".format('49,Have a nice day!'))
+
+print(f"For only {a1} dollars! {b2}".format(a1=49, b2='Have a nice day!'))
