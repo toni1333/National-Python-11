4ec84d6ff960eb16be839f799856e5c053e1676f
toni <antoniebohak@yahoo.com>
2022-07-06 20:27:41 +0300

    .
    .

--- Saptamana1/spanzuratoarea.py
+++ Saptamana1/spanzuratoarea.py
@@ -1,3 +1,9 @@
 cuvant = 'o n o m a t o p e e'
 cuvant = 'o _ o _ _ _ o _ e e'
 #7 incercari
+cuvant = list('onomatopee')
+for i in range(len(cuvant)):
+    print(cuvant[0], cuvant[-1])
+    if i != cuvant[0] or i != cuvant[-1]:
+        cuvant[i] = '_'
+print(cuvant)
