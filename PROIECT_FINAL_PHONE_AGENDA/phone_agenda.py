50b79acdcfb6fdae7bc4cf23921007daa2439643
toni <antoniebohak@yahoo.com>
2022-10-25 20:35:13 +0300

    .

--- PROIECT_FINAL_PHONE_AGENDA/phone_agenda.py
+++ PROIECT_FINAL_PHONE_AGENDA/phone_agenda.py
@@ -10,7 +10,7 @@ def open_file():
         reader = csv.reader(file, delimiter=',')
         for elem in reader:
             contact_list.append(elem)
-            set_select()
+            select_set()
     print(contact_list)
 
 
@@ -25,18 +25,18 @@ def save_file(contact_list):
 def selectare():
     print("hello", len(select.curselection()))
     if len(select.curselection()) == 0:
-        messagebox.showerror("EROARE!", "Va rugam introduce-ti nume!")
+        messagebox.showerror("EROARE!", "Selectati nume pentru incarcare date!")
     else:
         return int(select.curselection()[0])
 
 
-def AddDetail():
+def adddetail():
     if E_name.get() != "" and E_last_name.get() != "" and E_contact.get() != "":
         if E_contact.get().isdigit():
             contact_list.append([E_name.get() + ' ' + E_last_name.get(), E_contact.get()])
             save_file(contact_list)
-            set_select()
-            EntryReset()
+            select_set()
+            entryreset()
             messagebox.showinfo('CONFIRMARE!', "Contact nou adaugat cu SUCCES!")
         else:
             messagebox.showerror("EROARE!", "Va rugam introduce-ti un numarul de telefon valid!!")
@@ -46,13 +46,13 @@ def AddDetail():
     print('din add detail()',contact_list)
 
 
-def UpdateDetail():
+def updatedetail():
     if E_name.get() and E_last_name.get() and E_contact.get():
         contact_list[selectare()] = [E_name.get() + ' ' + E_last_name.get(), E_contact.get()]
         save_file(contact_list)
         messagebox.showinfo('CONFIRMARE!', "Contact actualizat cu SUCCES!")
-        EntryReset()
-        set_select()
+        entryreset()
+        select_set()
 
     elif not (E_name.get()) and not (E_last_name.get()) and not (E_contact.get()) and not (
             len(select.curselection()) == 0):
@@ -60,46 +60,45 @@ def UpdateDetail():
 
     else:
         if len(select.curselection()) == 0:
-            messagebox.showerror("EROARE!", "Va rugam selectati numele \n si apasati butonul de Incarcare")
+            messagebox.showerror("EROARE!", "Va rugam incarcati toate campurile \n si apasati butonul de Actualizare")
         else:
-            message1 = """Pentru incarcare date \n 
-		                    selectati campul si apasati Actualizare\n.
+            message1 = """Pentru incarcare date selectati campul si apasati Actualizare.
 						  """
             messagebox.showerror("Error", message1)
 
 
-def EntryReset():
+def entryreset():
     E_name_var.set('')
     E_last_name_var.set('')
     E_contact_var.set('')
 
 
-def DeleteEntry():
+def deleteEntry():
     if len(select.curselection()) != 0:
         result = messagebox.askyesno('CONFIRMARE!', 'DORITI SA STERGE-TI\n SELECTARE')
         if result == True:
             del contact_list[selectare()]
             save_file(contact_list)
-            set_select()
+            select_set()
     else:
         messagebox.showerror("EROARE!", 'Va rugam selectati numele')
 
 
-def LoadEntry():
+def loadentry():
     name, phone = contact_list[selectare()]
     print(name.split(' '))
     E_name_var.set(name.split()[0])
     E_last_name_var.set(name.split()[1])
     E_contact_var.set(phone)
 
-def ExitEntry():
+def exitentry():
     msg_box = messagebox.askquestion('IESIRE DIN APLICATIE!', 'Sunteti sigur ca vreti sa iesiti?',
                                         icon='info')
     if msg_box == 'yes':
         window.destroy()
 
 
-def set_select():
+def select_set():
     contact_list.sort(key=lambda elem:elem[0])
     select.delete(0, END)
     i = 0
@@ -140,13 +139,13 @@ E_contact.grid(row=2, column=1, padx=5, pady=5)
 Frame2 = Frame(window)
 Frame2.grid(row=0, column=1, padx=15, pady=15, sticky=E)
 
-Add_button = Button(Frame2, text="Adauga contact", width=15, bg="MediumSeaGreen", fg="black", command=AddDetail)
+Add_button = Button(Frame2, text="Adauga contact", width=15, bg="MediumSeaGreen", fg="black", command=adddetail)
 Add_button.grid(row=0, column=0, padx=8, pady=8)
 
-Update_button = Button(Frame2, text="Actualizeaza contact", width=15, bg="lightblue", fg="black", command=UpdateDetail)
+Update_button = Button(Frame2, text="Actualizeaza contact", width=15, bg="lightblue", fg="black", command=updatedetail)
 Update_button.grid(row=1, column=0, padx=8, pady=8)
 
-Reset_button = Button(Frame2, text="Reseteaza", width=15, bg="lightyellow", fg="black", command=EntryReset)
+Reset_button = Button(Frame2, text="Reseteaza", width=15, bg="lightyellow", fg="black", command=entryreset)
 Reset_button.grid(row=2, column=0, padx=8, pady=8)
 # ----------------------------------------------------------------------------
 
@@ -164,13 +163,13 @@ scroll.grid(row=0, column=1, sticky=N + S)
 ActionFrame = Frame(window)
 ActionFrame.grid(row=1, column=1, padx=15, pady=15, sticky=E)
 
-Delete_button = Button(ActionFrame, text="Sterge", width=15, bg="darkred", fg="black", command=DeleteEntry)
+Delete_button = Button(ActionFrame, text="Sterge", width=15, bg="darkred", fg="black", command=deleteEntry)
 Delete_button.grid(row=0, column=0, padx=5, pady=5, sticky=S)
 
-Loadbutton = Button(ActionFrame, text="Incarca editare", width=15, bg="DarkGoldenRod", fg="black", command=LoadEntry)
+Loadbutton = Button(ActionFrame, text="Incarca editare", width=15, bg="DarkGoldenRod", fg="black", command=loadentry)
 Loadbutton.grid(row=1, column=0, padx=5, pady=5)
 
-Exit_button = Button(ActionFrame, text="Iesire", width=15, bg="red", fg="white", command=ExitEntry)
+Exit_button = Button(ActionFrame, text="Iesire", width=15, bg="red", fg="white", command=exitentry)
 Exit_button.grid(row=2, column=0, padx=5, pady=5, sticky=S)
 
 # --------------------------------- de aici ruleaza programul
